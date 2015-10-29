57f2295d14fffc993344bdd80fdf6d84bfd9e9fd
JSterling8 <JSterling8@gmail.com>
2015-10-29 21:46:08 +0000

    Initial commit

---
+++ src/main/java/Result.java
@@ -0,0 +1,58 @@
+/**
+ * Created by anon on 29/10/2015.
+ */
+public class Result {
+    private String nameTeamOne;
+    private String nameTeamTwo;
+    private int scoreTeamOne;
+    private int scoreTeamTwo;
+    private boolean oneMatch;
+
+    public Result(String nameTeamOne, String nameTeamTwo, int scoreTeamOne, int scoreTeamTwo, boolean oneMatch) {
+        this.nameTeamOne = nameTeamOne;
+        this.nameTeamTwo = nameTeamTwo;
+        this.scoreTeamOne = scoreTeamOne;
+        this.scoreTeamTwo = scoreTeamTwo;
+        this.oneMatch = oneMatch;
+    }
+
+    public String getNameTeamOne() {
+        return nameTeamOne;
+    }
+
+    public void setNameTeamOne(String nameTeamOne) {
+        this.nameTeamOne = nameTeamOne;
+    }
+
+    public String getNameTeamTwo() {
+        return nameTeamTwo;
+    }
+
+    public void setNameTeamTwo(String nameTeamTwo) {
+        this.nameTeamTwo = nameTeamTwo;
+    }
+
+    public int getScoreTeamOne() {
+        return scoreTeamOne;
+    }
+
+    public void setScoreTeamOne(int scoreTeamOne) {
+        this.scoreTeamOne = scoreTeamOne;
+    }
+
+    public int getScoreTeamTwo() {
+        return scoreTeamTwo;
+    }
+
+    public void setScoreTeamTwo(int scoreTeamTwo) {
+        this.scoreTeamTwo = scoreTeamTwo;
+    }
+
+    public boolean isOneMatch() {
+        return oneMatch;
+    }
+
+    public void setOneMatch(boolean oneMatch) {
+        this.oneMatch = oneMatch;
+    }
+}
